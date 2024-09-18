49baa27da60f44c7fede7c8447e120d9bfb551de
l.serra <l.serra@white3.it>
2024-09-18 17:32:38 +0200

    Boh

---
+++ Assets/Scripts/Model/DataStructure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestLab.EventChannel.Model
+{
+    public class DataStructure<T> : IEventBasedData
+    {
+        public event Action OnDataReloaded;
+        public event Action OnDataClear;
+
+        private List<T> _data = new List<T>();
+
+        public List<T> Data
+        {
+            get => _data;
+            set
+            {
+                _data = value;
+                OnDataReloaded?.Invoke();
+            }
+        }
+
+        public void ResetData()
+        {
+            _data.Clear();
+            OnDataClear?.Invoke();
+        }
+    }
+}
